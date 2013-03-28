e96bc915e926605a0923f516e83c3e9c3a9bf3e9
Torkild Retvedt <torkild@retvedt.no>
2013-03-28 22:27:44 +0100

    Forgot to delete file, reformatted some

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var express = require('express')
-  , routes = require('./routes')
-  , display = require('./display');
+    , routes = require('./routes')
+    , display = require('./display');
 
 var app = module.exports = express();
 var port = 3000;
 
 // Configuration
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {layout: false});
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 var postHandle = function(data) {
@@ -43,6 +43,6 @@ app.post('/', function(req,res){
 });
 
 app.listen(port, function(){
-  console.log("server running on http://localhost:" + port);
+    console.log("server running on http://localhost:" + port);
 });
 
