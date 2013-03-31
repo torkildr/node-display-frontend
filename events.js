b4c0da7008eb16b213a1dec068ae8b8cd6dacc8a
Torkild Retvedt <torkild@retvedt.no>
2013-04-01 01:47:05 +0200

    Updates display according to active texts

--- events.js
+++ events.js
@@ -2,17 +2,34 @@ var display = require('./display');
 var util = require('./util');
 var database = require('./database');
 
-var interval = 5000;
+// seconds
+var interval = 5 * 1000;
+
+// makes sure to only update when text has changed
+var lastText = "";
+
+function updateText(text) {
+    if (text == lastText)
+        return;
+
+    display.scroll("auto");
+    display.text(text, true);
+    lastText = text;
+}
 
 var dispatcher = function() {
     var date = new Date();
     var seconds = util.convertToSeconds(date.getHours(), date.getMinutes(), date.getSeconds());
 
     database.getActiveTexts(seconds, function(err, rows) {
-        console.log("Active texts: " + rows.length);
+        var texts = rows.map(function (row) { return row.text; });
+        var formattedText = texts.join(" | ");
+        console.log("Active texts: " + texts.length);
+        updateText(formattedText);
     });
 };
 
+// keep track of the timer id
 var id;
 
 var stop = function() {
@@ -25,6 +42,7 @@ var start = function() {
     stop();
 
     id = setInterval(dispatcher, interval);
+    dispatcher();
 }
 
 exports.stop = stop;
